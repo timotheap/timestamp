2bc27110c349a53592ad40d5677221cb9ec598df
timotheap <vominhthuy85@gmail.com>
2017-01-20 04:07:36 +0000

    unix time

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+app.get('/:qs', function (req, res) {
+var querySearch = req.params.qs;
+
+if(querySearch.length === 10 && querySearch.match(/^[0-9]+$/) != null){
+var time = Number(querySearch);
+console.log(moment.unix(time).format("YYYY-MM-DD"));
+}else{
+    var year = querySearch.slice(querySearch.indexOf(",")+ 1);
+    console.log(year);
+    var month = querySearch.slice(querySearch.indexOf( /[a-zA-Z]/));
+    console.log(month);
+
+}
+});
+
+
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+  console.log('Example app listening on port 8080!');
+});
